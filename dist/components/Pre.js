09a27cfb3685419e44db279a9c1d807309cf8b80
carlosrivera <carlosrivera@users.noreply.github.com>
2021-03-08 11:19:42 +0000

    deploy: 7191be7d75e1a5b1914529f2782ff71af26699df

--- dist/components/Pre.js
+++
@@ -1,8 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import { jsx as _jsx } from "../../_snowpack/pkg/react/jsx-runtime.js";
-export default (({
-  props
-}) => {
-  return /*#__PURE__*/_jsx("div", { ...props
-  });
-});
