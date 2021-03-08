5c1a6af47bfc206afb30b5d397dfb221523b38ca
carlosrivera <carlosrivera@users.noreply.github.com>
2021-03-08 13:22:55 +0000

    deploy: eb95863aaf76e83e1ce609472e4b2f61b66ac513

--- _snowpack/env.js
+++
@@ -1,5 +0,0 @@
-export const SNOWPACK_PUBLIC_TITLE = "@synx-ai/gh-pages-template";
-export const SNOWPACK_PUBLIC_DESCRIPTION = "Template to genenerate a React GitHub page from a readme file.";
-export const MODE = "production";
-export const NODE_ENV = "production";
-export const SSR = false;
