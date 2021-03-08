1da4b0501a7592707701e7035f611802d871c4a7
Carlos Rivera <me@carlosrivera.im>
2021-03-08 03:57:27 -0600

    🐞 package not loading on external repo fixed

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,12 @@
-try {
-  const pkg = require("../package.json");
-} catch {
-  const pkg = require("./package.json");
+const fs = require("fs");
+const path = require("path");
+
+let pkg;
+
+if (fs.existsSync(path.resolve(process.cwd(), "../package.json"))) {
+  pkg = require("../package.json");
+} else {
+  pkg = require("./package.json");
 }
 
 process.env.SNOWPACK_PUBLIC_TITLE = pkg.name;
